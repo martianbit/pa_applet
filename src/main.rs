2c3ac807ea03720e4a416e5682004f3df7d91575
martianbit <martianbit@proton.me>
2023-01-20 19:24:52 +0100

    getting started

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use gtk::prelude::*;
+use gtk::Menu;
+use libappindicator::{AppIndicator, AppIndicatorStatus};
+
 fn main() {
-    println!("hello world");
+    gtk::init().unwrap();
+
+    let mut indicator = AppIndicator::new("pa applet", "audio-volume-high");
+    indicator.set_status(AppIndicatorStatus::Active);
+
+    let mut menu = Menu::new();
+
+    indicator.set_menu(&mut menu);
+    menu.show_all();
+
+    gtk::main();
 }
 
