952be7a5b2dd22461af8c0e15d71681b208fb4eb
martianbit <martianbit@proton.me>
2023-01-21 20:11:29 +0100

    use qt for the project instead

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,61 @@
-use gtk::prelude::*;
-use gtk::Menu;
-use libappindicator::{AppIndicator, AppIndicatorStatus};
+use std::rc::Rc;
+use cpp_core::Ptr;
+use cpp_core::CppBox;
+use cpp_core::StaticUpcast;
+use qt_core::QString;
+use qt_core::QObject;
+use qt_core::QPoint;
+use qt_core::QBox;
+use qt_core::SlotNoArgs;
+use qt_core::slot;
+use qt_gui::QCursor;
+use qt_gui::QIcon;
+use qt_widgets::QApplication;
+use qt_widgets::QSystemTrayIcon;
+
+const ICONS_NAME: &str = "Papirus-Dark";
+
+struct PaApplet {
+    tray_icon: QBox<QSystemTrayIcon>
+}
+
+impl StaticUpcast<QObject> for PaApplet {
+    unsafe fn static_upcast(ptr: Ptr<Self>) -> Ptr<QObject> {
+        ptr.tray_icon.as_ptr().static_upcast()
+    }
+}
+
+impl PaApplet {
+    unsafe fn new() -> Rc<Self> {
+        let tray_icon = QSystemTrayIcon::new();
+
+        tray_icon.set_icon(&QIcon::from_theme_1a(&QString::from_std_str("audio-volume-high")));
+        tray_icon.show();
+
+        let this = Rc::new(Self {
+            tray_icon
+        });
+
+        this.init();
+        this
+    }
+
+    unsafe fn init(self: &Rc<Self>) {
+        self.tray_icon.activated().connect(&self.slot_on_click());
+    }
+
+    #[slot(SlotNoArgs)]
+    unsafe fn on_click(self: &Rc<Self>) {
+        let point: CppBox<QPoint> = QCursor::pos_0a();
+        println!("{} {}", point.x(), point.y());
+    }
+}
 
 fn main() {
-    gtk::init().unwrap();
-
-    let mut indicator = AppIndicator::new("pa applet", "audio-volume-high");
-    indicator.set_status(AppIndicatorStatus::Active);
-
-    let mut menu = Menu::new();
-
-    indicator.set_menu(&mut menu);
-    menu.show_all();
-
-    gtk::main();
+    QApplication::init(|_| unsafe {
+        QIcon::set_theme_name(&QString::from_std_str(ICONS_NAME));
+        let _pa_applet = PaApplet::new();
+        QApplication::exec()
+    });
 }
 
