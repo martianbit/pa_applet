06f32faa4ec59bc31e06b382cd2c5cf7b5ce9319
martianbit <martianbit@proton.me>
2023-01-22 14:32:39 +0100

    make a new dialog every time

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,12 @@ use qt_widgets::QApplication;
 use qt_widgets::QSystemTrayIcon;
 
 const SINK_INDEX: &str = "0";
-const MAX_ABS_VOLUME: u32 = 65536;
+const MAX_ABS_VOLUME: u32 = 65537;
 const DIALOG_WIDTH: u32 = 200;
 const DIALOG_HEIGHT: u32 = 50;
 const DIALOG_POS_X: u32 = 1920 - DIALOG_WIDTH - 10;
 const DIALOG_POS_Y: u32 = 40;
 const ICONS_NAME: &str = "Papirus-Dark";
-
-static mut DIALOG: Option<Dialog> = None;
 
 fn set_volume(volume: u8) {
     let abs_volume = (volume as u32) * MAX_ABS_VOLUME / 100;
@@ -61,36 +59,31 @@ impl TrayIcon {
     }
 
     #[slot(SlotNoArgs)]
-    unsafe fn on_click(self: &Rc<Self>) {
-        let dialog = DIALOG.as_ref().unwrap();
+    fn on_click(self: &Rc<Self>) {
+        let scale = Scale::with_range(Orientation::Horizontal, 0_f64, 100_f64, 1_f64);
+
+        scale.connect_value_changed(|x| {
+            set_volume(x.value() as u8);
+        });
+
+        let dialog = Dialog::new();
+
+        dialog.move_(DIALOG_POS_X as i32, DIALOG_POS_Y as i32);
+        dialog.resize(DIALOG_WIDTH as i32, DIALOG_HEIGHT as i32);
+
+        dialog.content_area().pack_start(&scale, true, true, 0);
+
+        dialog.connect_focus_out_event(|x, _| {
+            x.emit_close();
+            Inhibit(true)
+        });
 
         dialog.show_all();
-        dialog.move_(DIALOG_POS_X as i32, DIALOG_POS_Y as i32);
     }
 }
 
 fn main() {
     gtk::init().unwrap();
-
-    let scale = Scale::with_range(Orientation::Horizontal, 0_f64, 100_f64, 1_f64);
-
-    scale.connect_value_changed(|x| {
-        set_volume(x.value() as u8);
-    });
-
-    let dialog = Dialog::new();
-
-    dialog.resize(DIALOG_WIDTH as i32, DIALOG_HEIGHT as i32);
-    dialog.content_area().pack_start(&scale, true, true, 0);
-
-    dialog.connect_focus_out_event(|x, _| {
-        x.hide();
-        Inhibit(false)
-    });
-
-    unsafe {
-        DIALOG = Some(dialog);
-    }
 
     QApplication::init(|_| unsafe {
         QIcon::set_theme_name(&QString::from_std_str(ICONS_NAME));
