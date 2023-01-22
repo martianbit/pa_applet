266b0f07c3c839b677f1ad18181d048ac9bf3d63
martianbit <martianbit@proton.me>
2023-01-22 13:18:23 +0100

    dialog shows then hides on focus out

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,47 @@
 use std::rc::Rc;
 use cpp_core::Ptr;
-use cpp_core::CppBox;
 use cpp_core::StaticUpcast;
+use gtk::prelude::*;
+use gtk::Dialog;
+use gtk::Scale;
+use gtk::Orientation;
+use gtk::Inhibit;
 use qt_core::QString;
 use qt_core::QObject;
-use qt_core::QPoint;
 use qt_core::QBox;
 use qt_core::SlotNoArgs;
 use qt_core::slot;
-use qt_gui::QCursor;
 use qt_gui::QIcon;
 use qt_widgets::QApplication;
 use qt_widgets::QSystemTrayIcon;
 
+const DIALOG_WIDTH: u32 = 200;
+const DIALOG_HEIGHT: u32 = 50;
+const DIALOG_POS_X: u32 = 1920 - DIALOG_WIDTH - 10;
+const DIALOG_POS_Y: u32 = 40;
 const ICONS_NAME: &str = "Papirus-Dark";
 
-struct PaApplet {
-    tray_icon: QBox<QSystemTrayIcon>
+static mut DIALOG: Option<Dialog> = None;
+
+struct TrayIcon {
+    widget: QBox<QSystemTrayIcon>
 }
 
-impl StaticUpcast<QObject> for PaApplet {
+impl StaticUpcast<QObject> for TrayIcon {
     unsafe fn static_upcast(ptr: Ptr<Self>) -> Ptr<QObject> {
-        ptr.tray_icon.as_ptr().static_upcast()
+        ptr.widget.as_ptr().static_upcast()
     }
 }
 
-impl PaApplet {
+impl TrayIcon {
     unsafe fn new() -> Rc<Self> {
-        let tray_icon = QSystemTrayIcon::new();
+        let widget = QSystemTrayIcon::new();
 
-        tray_icon.set_icon(&QIcon::from_theme_1a(&QString::from_std_str("audio-volume-high")));
-        tray_icon.show();
+        widget.set_icon(&QIcon::from_theme_1a(&QString::from_std_str("audio-volume-high")));
+        widget.show();
 
         let this = Rc::new(Self {
-            tray_icon
+            widget,
         });
 
         this.init();
@@ -41,20 +49,38 @@ impl PaApplet {
     }
 
     unsafe fn init(self: &Rc<Self>) {
-        self.tray_icon.activated().connect(&self.slot_on_click());
+        self.widget.activated().connect(&self.slot_on_click());
     }
 
     #[slot(SlotNoArgs)]
     unsafe fn on_click(self: &Rc<Self>) {
-        let point: CppBox<QPoint> = QCursor::pos_0a();
-        println!("{} {}", point.x(), point.y());
+        DIALOG.as_ref().unwrap().show_all();
     }
 }
 
 fn main() {
+    gtk::init().unwrap();
+
+    let dialog = Dialog::new();
+
+    dialog.move_(DIALOG_POS_X as i32, DIALOG_POS_Y as i32);
+    dialog.resize(DIALOG_WIDTH as i32, DIALOG_HEIGHT as i32);
+
+    let scale = Scale::with_range(Orientation::Horizontal, 0_f64, 100_f64, 1_f64);
+    dialog.content_area().pack_start(&scale, true, true, 0);
+
+    dialog.connect_focus_out_event(|_, _| unsafe {
+        DIALOG.as_ref().unwrap().hide();
+        Inhibit(false)
+    });
+
+    unsafe {
+        DIALOG = Some(dialog);
+    }
+
     QApplication::init(|_| unsafe {
         QIcon::set_theme_name(&QString::from_std_str(ICONS_NAME));
-        let _pa_applet = PaApplet::new();
+        let _tray_icon = TrayIcon::new();
         QApplication::exec()
     });
 }
